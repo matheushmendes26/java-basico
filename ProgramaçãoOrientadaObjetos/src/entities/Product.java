f22169d4fd6021cff42040a8b961d3ce4f5f1977
Matheus Mendes <matheush.1998@hotmail.com>
2023-03-07 15:36:59 -0300

    Organize exercises past.

--- ProgramaçãoOrientadaObjetos/src/entities/Product.java
+++ ProgramaçãoOrientadaObjetos/src/entities/Product.java
@@ -3,17 +3,51 @@ package entities;
 public class Product {
     public String name;
     public double price;
-    public int quantity;
+    public Integer quantity;
+
+    public Product(){
+
+    }
+
+    public Product(String name, double price, Integer quantity) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
 
     public double totalValueInStock(){
         double totalValue = price*quantity;
         return totalValue;
     }
-    public void addProducts(int quantity){
+    public void addProducts(Integer quantity){
         this.quantity += quantity;
         totalValueInStock();
     }
-    public void removeProducts(int quantity){
+    public void removeProducts(Integer quantity){
         this.quantity -= quantity;
         totalValueInStock();
     }
